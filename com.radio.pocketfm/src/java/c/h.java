c938b3ebdbde0cb74648331f934e9b192ba57111
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-02-27 15:52:28 +0530

    Add files via upload

---
+++ com.radio.pocketfm/src/java/c/h.java
@@ -0,0 +1,61 @@
+/*
+ * Decompiled with CFR 0.0.
+ * 
+ * Could not load the following classes:
+ *  c.c
+ *  java.io.IOException
+ *  java.lang.Class
+ *  java.lang.IllegalArgumentException
+ *  java.lang.Object
+ *  java.lang.String
+ *  java.lang.StringBuilder
+ */
+package c;
+
+import c.c;
+import c.s;
+import c.u;
+import java.io.IOException;
+
+public abstract class h
+implements s {
+    private final s a;
+
+    public h(s s2) {
+        if (s2 != null) {
+            this.a = s2;
+            return;
+        }
+        throw new IllegalArgumentException("delegate == null");
+    }
+
+    @Override
+    public u a() {
+        return this.a.a();
+    }
+
+    @Override
+    public void a_(c c2, long l2) throws IOException {
+        this.a.a_(c2, l2);
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.a.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        this.a.flush();
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(this.getClass().getSimpleName());
+        stringBuilder.append("(");
+        stringBuilder.append(this.a.toString());
+        stringBuilder.append(")");
+        return stringBuilder.toString();
+    }
+}
+
