a02d3a96166768345aa220ef60c7c72430dd1a38
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-02-28 10:56:20 +0530

    Update a.java

--- com.radio.pocketfm/src/java/com/airbnb/Lottie/c/a/a.java
+++ com.radio.pocketfm/src/java/com/airbnb/Lottie/c/a/a.java
@@ -1,1 +1,26 @@
+/*
+ * Decompiled with CFR 0.0.
+ * 
+ * Could not load the following classes:
+ *  java.lang.Integer
+ *  java.lang.String
+ *  java.util.List
+ */
+package com.airbnb.lottie.c.a;
+
+import com.airbnb.lottie.a.b.b;
+import com.airbnb.lottie.c.a.n;
+import java.util.List;
+
+public class a
+extends n<Integer, Integer> {
+    public a(List<com.airbnb.lottie.g.a<Integer>> list) {
+        super(list);
+    }
+
+    @Override
+    public com.airbnb.lottie.a.b.a<Integer, Integer> a() {
+        return new b((List<com.airbnb.lottie.g.a<Integer>>)this.a);
+    }
+}
 
